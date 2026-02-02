3c49133cb590d9f339462b4aebf79a82c0bf5ee0
Mainn <nguyennhatmai@Bon-Cau-Bay.local>
2026-02-02 09:52:15 +0700

    submit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [nen, setNen] = useState('#6ea6e2');
+
+  return(
+    <View style={[styles.container, {backgroundColor: nen}]}>
+
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: '#6ea6e2'}]}
+        onPress={() => setNen('#6ea6e2')}
+        >
+          <Text style={styles.buttonText}>Blue</Text>
+        </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: '#be3d3b'}]}
+        onPress={() => setNen('#be3d3b')}
+        >
+          <Text style={styles.buttonText}>Red</Text>
+        </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: 'black'}]}
+        onPress={() => setNen('black')}
+        >
+          <Text style={styles.buttonText}>Black</Text>
+        </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: '#e783b8'}]}
+        onPress={() => setNen('#e783b8')}
+        >
+          <Text style={styles.buttonText}>Pink</Text>
+        </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: '#479d47'}]}
+        onPress={() => setNen('#479d47')}
+        >
+          <Text style={styles.buttonText}>Green</Text>
+        </TouchableOpacity>
+      <TouchableOpacity
+        style={[styles.button, {backgroundColor: '#f2f55e'}]}
+        onPress={() => setNen('#f2f55e')}
+        >
+          <Text style={styles.buttonText}>Yellow</Text>
+        </TouchableOpacity>
+
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  button: {
+    backgroundColor: '#007BFF',
+    borderRadius: 10,
+    marginVertical: 10,
+    width: 250,
+    height: 60,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
   },
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+#Họ tên: Nguyễn Nhật Mai
+#Msv: 23810310266
+
+![alt text](image.png)
